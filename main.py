fbb8fbab02a00ee45a5654501cd155e0a89314ca
mendi kuperman <mendikuperman@gmail.com>
2025-06-24 14:47:33 +0300

    prints line if num is found in line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-with open("my_text.txt" ,"a") as flehandlr:
+# 1
+with open("my_text.txt" ,"w") as flehandlr:
     flehandlr.write("helo world\n")
 
     flehandlr.write("my_text.txt")
@@ -12,6 +13,13 @@ with open("my_text.txt" ,"a") as flehandlr:
     flehandlr.write("it is exciting\n")
 
     flehandlr.write("And i am all 4 it")
+
+
+# 2
+with open("my_text.txt" ,"r") as flehandlr:
+    for line in flehandlr:
+         if any(char.isdigit() for char in line):
+            print(line.strip())
 
 
 
