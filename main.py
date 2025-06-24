6a8798cff5ee4c72e225f0951dc578ab6d086def
mendi kuperman <mendikuperman@gmail.com>
2025-06-24 14:36:51 +0300

    first writing to  the file

---
+++ main.py
@@ -0,0 +1,19 @@
+with open("my_text.txt" ,"a") as flehandlr:
+    flehandlr.write("helo world\n")
+
+    flehandlr.write("my_text.txt")
+
+    flehandlr.write("That mean it is number 1\n")
+
+    flehandlr.write("Not 2\n")
+
+    flehandlr.write("Not three\n")
+
+    flehandlr.write("it is exciting\n")
+
+    flehandlr.write("And i am all 4 it")
+
+
+
+
+
